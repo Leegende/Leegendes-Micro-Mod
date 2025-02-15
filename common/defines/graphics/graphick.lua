0a2df3f0b5aa71ac85f4198f36b9f1dea8e3d01b
Leegende <leqitknxckbackjr@gmail.com>
2025-02-15 13:49:56 +0100

    a

--- common/defines/graphics/graphick.lua
+++ common/defines/graphics/graphick.lua
@@ -1,94 +1,115 @@
-NDefines_Graphics.NGraphics.BORDER_WIDTH = 1.4
-NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 1.0
-NDefines_Graphics.NGraphics.TREE_FADE_NEAR = 0
-NDefines_Graphics.NGraphics.TREE_FADE_FAR = 0
-NDefines_Graphics.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
-NDefines_Graphics.NGraphics.BLOOM_WIDTH = 0
-NDefines_Graphics.NGraphics.BLOOM_SCALE = 0
-NDefines_Graphics.NGraphics.BRIGHT_THRESHOLD = 0
-NDefines_Graphics.NGraphics.EMISSIVE_BLOOM_STRENGTH = 0
-NDefines_Graphics.NGraphics.DRAW_SHADOWS_CUTOFF = 0
-NDefines_Graphics.NGraphics.DRAW_SHADOWS_FADE_LENGTH = 0
-NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0
-NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0
-NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.65 -- 0.6
-NDefines.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8 -- 0.8
+NDefines.NGraphics.WEATHER_DISTANCE_CUTOFF = 0  -- At what distance weather effects are hidden
+NDefines.NGraphics.WEATHER_DISTANCE_FADE_LENGTH = 400  -- How far the fade-out distance should be
+NDefines.NGraphics.WEATHER_ZOOM_IN_CUTOFF = 1  -- At what distance weather effects are faded out the most when zooming in
+NDefines.NGraphics.WEATHER_ZOOM_IN_FADE_LENGTH = 220  -- How far the zoom-in fade-out distance should be
+NDefines.NGraphics.WEATHER_ZOOM_IN_FADE_FACTOR = 0.0  -- How much the weather effects should fade out when maximum zoomed in
+NDefines.NGraphics.WEATHER_PLAYBACK_RATE = 0.25  -- Playback rate at maximum distance
+NDefines.NGraphics.WEATHER_PLAYBACK_RATE_CUTOFF = 400  -- Playback rate maximum distance
+NDefines.NGraphics.WEATHER_PLAYBACK_RATE_LENGTH = 200  -- For how long to fade between normal playback rate and maximum distance playback rate
+NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 1500}
 
-
-NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 200.0 --90 camera distance at which the icons begin to group up
-NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 500.0 --180 Camera distance at which the icons begin to group up on state level
-NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 1000 --350 second camera distance at which the icons begin to group up
-NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250 --230
-NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 300
-NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
-NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_NEAR = 250
-NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_FAR = 350
-NDefines_Graphics.NGraphics.STATE_BORDER_FADE_NEAR = 450
-NDefines_Graphics.NGraphics.STATE_BORDER_FADE_FAR = 550
-
-NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 300
-NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF = 300.0 -- railways are cut off above this camera height
-
--- NDefines_Graphics.NGraphics.WEATHER_DISTANCE_CUTOFF = 0 -- At what distance weather effects are hidden
--- NDefines_Graphics.NGraphics.WEATHER_ZOOM_IN_CUTOFF = 0 -- At what distance weather effects are faded out the most when zooming in
--- NDefines_Graphics.NGraphics.WEATHER_ZOOM_IN_FADE_LENGTH = 0 -- How far the zoom in fade out distance should be
--- NDefines_Graphics.NGraphics.WEATHER_ZOOM_IN_FADE_FACTOR = 0 -- How much the weather effects should fade out when maximum zoomed in
-
-NFrontend = {
-	CAMERA_LOOKAT_X = 2958.0, 						-- Rotation point in main menu
-	CAMERA_LOOKAT_Y = 0.0,
-	CAMERA_LOOKAT_Z = 1519.0,
-	CAMERA_START_X = 2958.0,						-- Initial position in main menu
-	CAMERA_START_Y = 800,							-- Y is height
-	CAMERA_START_Z = 1400.0,
-	CAMERA_END_X = 2958.0,							-- Move to position in main menu
-	CAMERA_END_Y = 900.0,
-	CAMERA_END_Z = 1400.0,
-	CAMERA_MIN_HEIGHT = 50.0,						-- Minimum camera height
-	CAMERA_MAX_HEIGHT = 3000.0,						-- Maximum camera height
-	CAMERA_SPEED_IN_MENUS = 0.1,
-	CAMERA_INTERPOLATION_SPEED = 0.19,
-}
-
+NDefines.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 100  -- Group moving and still units
+NDefines.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 400  -- Group into states
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 500  -- Group units into air regions
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250  -- Size limit for air region grouping
+NDefines.NGraphics.MAP_ICONS_STATE_HUGE = 100  -- Size limit for state grouping
+NDefines.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines.NGraphics.MAP_ICONS_GROUP_SPLIT_SELECTED_LIMIT = 10
+NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 250
+NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
 
 NDefines.NGraphics.COMMANDGROUP_PRESET_COLORS_HSV = {
-    00.0/360.0, 1.0, 1.0, --red
-    30.0/360.0, 1.0, 1.0, --orange
-    60.0/360.0, 1.0, 1.0, --yellow
-    120.0/360.0, 1.0, 1.0, --green
-    180.0/360.0, 1.0, 1.0, --cyan
-    240.0/360.0, 1.0, 1.0, --blue
-    270.0/360.0, 1.0, 1.0, --purple
-    0.0/360.0, 0.0, 1.0, --white
-    00.0/360.0, 1.0, 0.0 --black
+    0.0 / 360.0, 1.0, 1.0,  -- Red
+    10.0 / 360.0, 1.0, 1.0,  -- Orange
+    60.0 / 360.0, 1.0, 1.0,  -- Yellow
+    120.0 / 360.0, 0.75, 1.0,  -- Green
+    180.0 / 360.0, 1.0, 1.0,  -- Turquoise
+    235.0 / 360.0, 1.0, 1.0,  -- Blue
+    260.0 / 360.0, 1.0, 1.0,  -- Dark purple
+    300.0 / 360.0, 1.0, 1.0,  -- Light purple
+    330.0 / 360.0, 0.0, 1.0  -- White
 }
 
-NDefines.NGraphics.MIN_TRAIN_WAGON_COUNT = 1
-NDefines.NGraphics.MAX_TRAIN_WAGON_COUNT = 1
-NDefines.NGraphics.CITY_SPRAWL_SHRINK_DISTANCE = 0
-NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 350 			-- Remove province names beyond this distance
-NDefines.NGraphics.DRAW_COUNTRY_NAMES_CUTOFF = 500
+NDefines.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
 
-NDefines.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 50 -- 10
+NDefines.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 900
+NDefines.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 900
 
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 250.0
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 250
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 250
 
-NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {200, 350, 500}  --150 250 500
-NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF_MIN = 100.0 -- Min range for victory point text
-NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF_MAX = 500.0 --800 Max range for victory point text
-NDefines.NGraphics.VICTORY_POINT_MAP_ICON_DOT_CUTOFF_MIN = 100.0 -- Min range for victory point dot
-NDefines.NGraphics.VICTORY_POINT_MAP_ICON_DOT_CUTOFF_MAX = 500.0 --1000 Max range for victory point text
+NDefines.NGraphics.ROOT_FRONT_OFFSET = 2
+NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.70
+NDefines.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.80
 
-NDefines.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 400 --900 At what distance air bases are hidden
-NDefines.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 400 -- 900, -- At what distance naval bases are hidden
+NDefines_Graphics.NAirGfx_MAX_MISSILE_BOMBING_SCENARIOS = 0
+NDefines_Graphics.NAirGfx_MAX_BOMBING_SCENARIOS = 0
+NDefines_Graphics.NAirGfx_MAX_PATROL_SCENARIOS = 0
+NDefines_Graphics.NAirGfx_MAX_DOGFIGHTS_SCENARIOS = 0
+NDefines_Graphics.NAirGfx_MAX_TRANSPORT_SCENARIOS = 0
+NDefines_Graphics.NAirGfx_AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1}
+NDefines_Graphics.NAirGfx_AIRPLANES_SMOOTH_INTERPOLATION_MOVE = 0.02
+NDefines_Graphics.NAirGfx_AIRPLANES_SMOOTH_INTERPOLATION_TURN = 0.02
 
-NDefines.NGraphics.RADAR_ICON_DISTANCE_CUTOFF = 500  --1100 At what distance the radars are hidden
-NDefines.NGraphics.UNITS_DISTANCE_CUTOFF = 250 --120
+NDefines_Graphics.NMapMode_MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
+NDefines_Graphics.NMapMode_RADAR_ROTATION_SPEED = 0.0
+NDefines_Graphics.NMapMode_AIR_RANGE_INDICATOR_ROTATION_SPEED = 0.0000
 
+NDefines_Graphics.NGraphics_MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines_Graphics.NGraphics_MAP_ICONS_STATE_HUGE = 100
+NDefines_Graphics.NGraphics_GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 50
+NDefines_Graphics.NGraphics_DECISION_MAP_ICON_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics_NAVAL_COMBAT_DISTANCE_CUTOFF = 599
+NDefines_Graphics.NGraphics_PROVINCE_ANIM_TEXT_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics_AIRBASE_ICON_DISTANCE_CUTOFF = 800
+NDefines_Graphics.NGraphics_NAVALBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics_RADAR_ICON_DISTANCE_CUTOFF = 499
+NDefines_Graphics.NGraphics_VICTORY_POINTS_DISTANCE_CUTOFF = {250, 450, 600}
+NDefines_Graphics.NGraphics_MAX_MESHES_LOADED_PER_FRAME = 5
+NDefines_Graphics.NGraphics_DRAW_DETAILED_CUTOFF = 100
+NDefines_Graphics.NGraphics_UNITS_DISTANCE_CUTOFF = 350
+NDefines_Graphics.NGraphics_SHIPS_DISTANCE_CUTOFF = 50
+NDefines_Graphics.NGraphics_UNIT_ARROW_DISTANCE_CUTOFF = 500
+NDefines_Graphics.NGraphics_UNITS_ICONS_DISTANCE_CUTOFF = 400
+NDefines_Graphics.NGraphics_NAVAL_COMBAT_DISTANCE_CUTOFF = 999
+NDefines_Graphics.NGraphics_ADJACENCY_RULE_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics_LAND_COMBAT_DISTANCE_CUTOFF = 700
+NDefines_Graphics.NGraphics_SUPPLY_ICON_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics_PROV_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 300
+NDefines_Graphics.NGraphics_STATE_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics_GRADIENT_BORDERS_REFRESH_FREQ = 0.2
+NDefines_Graphics.NGraphics_MAPICON_GROUP_PASSES = 5
+NDefines_Graphics.NGraphics_DRAW_REFRACTIONS_CUTOFF = 0
+NDefines_Graphics.NGraphics_BLOOM_WIDTH = 0
+NDefines_Graphics.NGraphics_BLOOM_SCALE = 0
+NDefines_Graphics.NGraphics_BRIGHT_THRESHOLD = 0
+NDefines_Graphics.NGraphics_EMISSIVE_BLOOM_STRENGTH = 0
+NDefines_Graphics.NGraphics_DAY_NIGHT_FEATHER = 0.024
+NDefines_Graphics.NGraphics_DRAW_SHADOWS_CUTOFF = 0
+NDefines_Graphics.NGraphics_DRAW_SHADOWS_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics_DRAW_FOW_CUTOFF = 0
+NDefines_Graphics.NGraphics_DRAW_FOW_FADE_LENGTH = 0
 
-NDefines.NGraphics.RAILWAY_Y_OFFSET = 0.9							-- Railways are offset by this amount vertically from the map
-NDefines.NGraphics.RAILWAY_BRIDGE_Y_OFFSET = 0.6					-- Railway bridges are offset by this amount vertically from the map
-NDefines.NGraphics.RAILWAY_BRIDGE_WIDTH = 0						-- Railways will have straight segments of this length for regular bridges
-NDefines.NGraphics.RAILWAY_BRIDGE_LARGE_WIDTH = 0				-- Railways will have straight segments of this length for large bridges
-NDefines.NGraphics.RAILWAY_BRIDGE_GAP_WIDTH = 0					-- Railways will have gaps of this length for regular bridges
-NDefines.NGraphics.RAILWAY_BRIDGE_GAP_LARGE_WIDTH = 0			-- Railways will have gaps of this length for large bridges
-NDefines.NGraphics.COARSE_RAILWAY_GUN_POSITION_OFFSET = { 0, 0 }			-- Coarse railway gun icons will have their world centers offset by this offset
+NDefines_Graphics.NInterface_GRIDBOX_ELEMENTS_INTERPOLATION_SPEED = 0.2
+
+NDefines_Graphics.NFrontend_CAMERA_LOOKAT_X = 2958.0
+NDefines_Graphics.NFrontend_CAMERA_LOOKAT_Y = 0.0
+NDefines_Graphics.NFrontend_CAMERA_LOOKAT_Z = 1519.0
+NDefines_Graphics.NFrontend_CAMERA_START_X = 2958.0
+NDefines_Graphics.NFrontend_CAMERA_START_Y = 800
+NDefines_Graphics.NFrontend_CAMERA_START_Z = 1400.0
+NDefines_Graphics.NFrontend_CAMERA_END_X = 2958.0
+NDefines_Graphics.NFrontend_CAMERA_END_Y = 900.0
+NDefines_Graphics.NFrontend_CAMERA_END_Z = 1400.0
+NDefines_Graphics.NFrontend_CAMERA_MIN_HEIGHT = 50.0
+NDefines_Graphics.NFrontend_CAMERA_MAX_HEIGHT = 3000.0
+NDefines_Graphics.NFrontend_CAMERA_SPEED_IN_MENUS = 0.1
+NDefines_Graphics.NFrontend_CAMERA_INTERPOLATION_SPEED = 0.19
+
+NDefines_Graphics.NGraphics_MIN_TRAIN_WAGON_COUNT = 1
+NDefines_Graphics.NGraphics_MAX_TRAIN_WAGON_COUNT = 1
+NDefines_Graphics.NGraphics_CITY_SPRAWL_SHRINK_DISTANCE = 0
+NDefines_Graphics.NGraphics_PROVINCE_NAME_DRAW_DISTANCE = 350
+NDefines_Graphics.NGraphics_DRAW_COUNTRY_NAMES_CUTOFF = 500
+NDefines_Graphics.NGraphics_MAX_MESHES_LOADED_PER_FRAME = 50
