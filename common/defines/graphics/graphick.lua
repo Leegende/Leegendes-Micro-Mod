46a32f45a1b496d91fe466ae31e8f55680914877
Leegende <leqitknxckbackjr@gmail.com>
2025-03-29 18:16:58 +0100

    a

--- common/defines/graphics/graphick.lua
+++ common/defines/graphics/graphick.lua
@@ -1,47 +1,37 @@
-NDefines.NGraphics.WEATHER_DISTANCE_CUTOFF = 0  -- At what distance weather effects are hidden
-NDefines.NGraphics.WEATHER_DISTANCE_FADE_LENGTH = 400  -- How far the fade-out distance should be
-NDefines.NGraphics.WEATHER_ZOOM_IN_CUTOFF = 1  -- At what distance weather effects are faded out the most when zooming in
-NDefines.NGraphics.WEATHER_ZOOM_IN_FADE_LENGTH = 220  -- How far the zoom-in fade-out distance should be
-NDefines.NGraphics.WEATHER_ZOOM_IN_FADE_FACTOR = 0.0  -- How much the weather effects should fade out when maximum zoomed in
-NDefines.NGraphics.WEATHER_PLAYBACK_RATE = 0.25  -- Playback rate at maximum distance
-NDefines.NGraphics.WEATHER_PLAYBACK_RATE_CUTOFF = 400  -- Playback rate maximum distance
-NDefines.NGraphics.WEATHER_PLAYBACK_RATE_LENGTH = 200  -- For how long to fade between normal playback rate and maximum distance playback rate
+NDefines.NGraphics.WEATHER_DISTANCE_CUTOFF = 0
+NDefines.NGraphics.WEATHER_DISTANCE_FADE_LENGTH = 400
+NDefines.NGraphics.WEATHER_ZOOM_IN_CUTOFF = 1
+NDefines.NGraphics.WEATHER_ZOOM_IN_FADE_LENGTH = 220
+NDefines.NGraphics.WEATHER_ZOOM_IN_FADE_FACTOR = 0.0
+NDefines.NGraphics.WEATHER_PLAYBACK_RATE = 0.25
+NDefines.NGraphics.WEATHER_PLAYBACK_RATE_CUTOFF = 400
+NDefines.NGraphics.WEATHER_PLAYBACK_RATE_LENGTH = 200
 NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 1500}
-
-NDefines.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 100  -- Group moving and still units
-NDefines.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 400  -- Group into states
-NDefines.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 500  -- Group units into air regions
-NDefines.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250  -- Size limit for air region grouping
-NDefines.NGraphics.MAP_ICONS_STATE_HUGE = 100  -- Size limit for state grouping
+NDefines.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 100
+NDefines.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 400
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 500
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250
+NDefines.NGraphics.MAP_ICONS_STATE_HUGE = 100
 NDefines.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
 NDefines.NGraphics.MAP_ICONS_GROUP_SPLIT_SELECTED_LIMIT = 10
 NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 250
 NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
-
-NDefines.NGraphics.COMMANDGROUP_PRESET_COLORS_HSV = {
-    0.0 / 360.0, 1.0, 1.0,  -- Red
-    10.0 / 360.0, 1.0, 1.0,  -- Orange
-    60.0 / 360.0, 1.0, 1.0,  -- Yellow
-    120.0 / 360.0, 0.75, 1.0,  -- Green
-    180.0 / 360.0, 1.0, 1.0,  -- Turquoise
-    235.0 / 360.0, 1.0, 1.0,  -- Blue
-    260.0 / 360.0, 1.0, 1.0,  -- Dark purple
-    300.0 / 360.0, 1.0, 1.0,  -- Light purple
-    330.0 / 360.0, 0.0, 1.0  -- White
-}
-
+NDefines.NGraphics.COMMANDGROUP_PRESET_COLORS_HSV = { 0.0/360.0, 1.0, 1.0, 10.0/360.0, 1.0, 1.0, 60.0/360.0, 1.0, 1.0, 120.0/360.0, 0.75, 1.0, 180.0/360.0, 1.0, 1.0, 235.0/360.0, 1.0, 1.0, 260.0/360.0, 1.0, 1.0, 300.0/360.0, 1.0, 1.0, 330.0/360.0, 0.0, 1.0 }
 NDefines.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
-
 NDefines.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 900
 NDefines.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 900
-
 NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 250.0
 NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 250
 NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 250
-
 NDefines.NGraphics.ROOT_FRONT_OFFSET = 2
 NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.70
 NDefines.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.80
+NDefines.NGraphics.MIN_TRAIN_WAGON_COUNT = 1
+NDefines.NGraphics.MAX_TRAIN_WAGON_COUNT = 1
+NDefines.NGraphics.CITY_SPRAWL_SHRINK_DISTANCE = 0
+NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 350
+NDefines.NGraphics.DRAW_COUNTRY_NAMES_CUTOFF = 500
+NDefines.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 50
 
 NDefines_Graphics.NAirGfx_MAX_MISSILE_BOMBING_SCENARIOS = 0
 NDefines_Graphics.NAirGfx_MAX_BOMBING_SCENARIOS = 0
@@ -51,11 +41,9 @@ NDefines_Graphics.NAirGfx_MAX_TRANSPORT_SCENARIOS = 0
 NDefines_Graphics.NAirGfx_AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1}
 NDefines_Graphics.NAirGfx_AIRPLANES_SMOOTH_INTERPOLATION_MOVE = 0.02
 NDefines_Graphics.NAirGfx_AIRPLANES_SMOOTH_INTERPOLATION_TURN = 0.02
-
 NDefines_Graphics.NMapMode_MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
 NDefines_Graphics.NMapMode_RADAR_ROTATION_SPEED = 0.0
 NDefines_Graphics.NMapMode_AIR_RANGE_INDICATOR_ROTATION_SPEED = 0.0000
-
 NDefines_Graphics.NGraphics_MAPICON_GROUP_STRATEGIC_SIZE = 1000
 NDefines_Graphics.NGraphics_MAP_ICONS_STATE_HUGE = 100
 NDefines_Graphics.NGraphics_GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 50
@@ -90,9 +78,7 @@ NDefines_Graphics.NGraphics_DRAW_SHADOWS_CUTOFF = 0
 NDefines_Graphics.NGraphics_DRAW_SHADOWS_FADE_LENGTH = 0
 NDefines_Graphics.NGraphics_DRAW_FOW_CUTOFF = 0
 NDefines_Graphics.NGraphics_DRAW_FOW_FADE_LENGTH = 0
-
 NDefines_Graphics.NInterface_GRIDBOX_ELEMENTS_INTERPOLATION_SPEED = 0.2
-
 NDefines_Graphics.NFrontend_CAMERA_LOOKAT_X = 2958.0
 NDefines_Graphics.NFrontend_CAMERA_LOOKAT_Y = 0.0
 NDefines_Graphics.NFrontend_CAMERA_LOOKAT_Z = 1519.0
@@ -106,10 +92,48 @@ NDefines_Graphics.NFrontend_CAMERA_MIN_HEIGHT = 50.0
 NDefines_Graphics.NFrontend_CAMERA_MAX_HEIGHT = 3000.0
 NDefines_Graphics.NFrontend_CAMERA_SPEED_IN_MENUS = 0.1
 NDefines_Graphics.NFrontend_CAMERA_INTERPOLATION_SPEED = 0.19
-
 NDefines_Graphics.NGraphics_MIN_TRAIN_WAGON_COUNT = 1
 NDefines_Graphics.NGraphics_MAX_TRAIN_WAGON_COUNT = 1
 NDefines_Graphics.NGraphics_CITY_SPRAWL_SHRINK_DISTANCE = 0
 NDefines_Graphics.NGraphics_PROVINCE_NAME_DRAW_DISTANCE = 350
 NDefines_Graphics.NGraphics_DRAW_COUNTRY_NAMES_CUTOFF = 500
 NDefines_Graphics.NGraphics_MAX_MESHES_LOADED_PER_FRAME = 50
+NDefines_Graphics.NMapIcons.STRATEGIC_AIR_PRIORITY_AIR_MISSION = 290
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 100
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 450
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 700
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 220
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_HUGE = 100
+NDefines_Graphics.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_SPLIT_SELECTED_LIMIT = 10
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 125
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 350
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_BAD = {0.65, 0, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_GOOD = {0, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_AVERAGE = {0.65, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_NEUTRAL = {130.0/255, 130.0/255, 130.0/255, 1}
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 250.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 250
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 250
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_GOOD = {0.0, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_AVERAGE = {0.65, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_BAD = {0.65, 0, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_NAVY_COLOR_MISSION = {0.65, 0.65, 0.0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_NAVY_COLOR_NEUTRAL = {130.0/255, 130.0/255, 130.0/255, 1}
+NDefines_Graphics.NGraphics.ROOT_FRONT_OFFSET = 2
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 1.4
+NDefines_Graphics.NGraphics.TREE_FADE_NEAR = 0
+NDefines_Graphics.NGraphics.TREE_FADE_FAR = 0
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_NEAR = 250
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_FAR = 350
+NDefines_Graphics.NGraphics.STATE_BORDER_FADE_NEAR = 450
+NDefines_Graphics.NGraphics.STATE_BORDER_FADE_FAR = 550
+NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 300
+NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF = 300.0
+NDefines_Graphics.NGraphics.RAILWAY_Y_OFFSET = 0.9
+NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_Y_OFFSET = 0.6
+NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_WIDTH = 0
+NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_LARGE_WIDTH = 0
+NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_GAP_WIDTH = 0
+NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_GAP_LARGE_WIDTH = 0
+NDefines_Graphics.NGraphics.COARSE_RAILWAY_GUN_POSITION_OFFSET = { 0, 0 }
