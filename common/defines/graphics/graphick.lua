f97c35ba16ccfaf3f96419cf983aafe174e49408
Leegende <leqitknxckbackjr@gmail.com>
2025-08-03 21:09:35 +0200

    Initial commit – clean history

---
+++ common/defines/graphics/graphick.lua
@@ -0,0 +1,166 @@
+NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.85 -- 0.6
+NDefines.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8 -- 0.8
+NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 1
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
+NDefines_Graphics.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_HUGE = 100
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 30
+NDefines_Graphics.NGraphics.DECISION_MAP_ICON_DISTANCE_CUTOFF = 100
+NDefines_Graphics.NGraphics.NAVAL_COMBAT_DISTANCE_CUTOFF = 1200
+NDefines_Graphics.NGraphics.PROVINCE_ANIM_TEXT_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.RADAR_ICON_DISTANCE_CUTOFF = 150
+NDefines_Graphics.NAirGfx.AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.3, 0.3, 0.3, 0.3, 0.3, 0.3}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {300, 350, 500}
+NDefines_Graphics.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 10
+NDefines_Graphics.NMapMode.RADAR_ROTATION_SPEED = 0.0
+NDefines_Graphics.NMapMode.AIR_RANGE_INDICATOR_ROTATION_SPEED = 0.0000
+NDefines_Graphics.NAirGfx.AIRPLANES_SMOOTH_INTERPOLATION_MOVE = 0.02
+NDefines_Graphics.NAirGfx.AIRPLANES_SMOOTH_INTERPOLATION_TURN = 0.02
+NDefines_Graphics.NInterface.GRIDBOX_ELEMENTS_INTERPOLATION_SPEED = 0.2
+NDefines_Graphics.NGraphics.RESOURCE_MAP_ICON_TEXT_CUTOFF = 400
+NDefines_Graphics.NGraphics.UNITS_DISTANCE_CUTOFF = 170
+NDefines_Graphics.NGraphics.SHIPS_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics.UNIT_ARROW_DISTANCE_CUTOFF = 500
+NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 700
+NDefines_Graphics.NGraphics.ADJACENCY_RULE_DISTANCE_CUTOFF = 1300
+NDefines_Graphics.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 800
+NDefines_Graphics.NGraphics.SUPPLY_ICON_DISTANCE_CUTOFF = 1500
+NDefines_Graphics.NGraphics.PROV_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 300
+NDefines_Graphics.NGraphics.STATE_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_REFRESH_FREQ = 0.2
+NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 5
+NDefines_Graphics.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+NDefines_Graphics.NGraphics.WEATHER_ZOOM_IN_CUTOFF = 500
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 500
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 800
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 1000
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 0
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
+NDefines_Graphics.NGraphics.EMISSIVE_BLOOM_STRENGTH = 0
+NDefines_Graphics.NGraphics.DAY_NIGHT_FEATHER = 0.024
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 475
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 100
+
+NDefines.NMapMode.SUPPLY_MAP_MODE_REACH_COLOR = {
+    0.0,   0.45, 0.00, 0.20, 1.0,
+    0.02,  0.35, 0.12, 0.45, 1.0,
+    0.12,  0.20, 0.15, 0.45, 1.0,
+    0.2,   0.27, 0.27, 0.50, 1.0,
+    0.4,   0.15, 0.25, 0.40, 1.0,
+    0.6,   0.20, 0.42, 0.60, 1.0,
+    0.8,   0.35, 0.50, 0.30, 1.0,
+    1.0,   0.15, 0.54, 0.15, 1.0,
+}
+
+NDefines.NGraphics.BORDER_COLOR_DEMILITARIZED_A = 0
+NDefines_Graphics.NMapIcons.STRATEGIC_AIR_PRIORITY_AIR_MISSION = 290
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 1500}
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_SPLIT_SELECTED_LIMIT = 10
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 125
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 350
+
+NDefines.NGraphics.COMMANDGROUP_PRESET_COLORS_HSV = {
+	000.0/360.0, 0.00, 0.95, -- Terrains
+	005.0/360.0, 0.05, 0.38,
+	000.0/360.0, 0.00, 0.15,
+	000.0/360.0, 1.00, 0.65,
+	030.0/360.0, 0.75, 0.9,
+	045.0/360.0, 0.45, 0.95,
+	030.0/360.0, 0.40, 0.38,
+	085.0/360.0, 0.40, 0.38,
+	155.0/360.0, 0.40, 0.38,
+	180.0/360.0, 0.80, 0.85,
+	210.0/360.0, 0.40, 0.85,
+	220.0/360.0, 0.30, 0.95,
+	0, 0.00, 1.00, -- White Scale Right
+	0, 0.00, 0.90, -- White Scale Right
+	000.0/360.0, 0.25, 0.95, -- Pale
+	030.0/360.0, 0.25, 0.95,
+	060.0/360.0, 0.25, 0.95,
+	090.0/360.0, 0.25, 0.95,
+	120.0/360.0, 0.25, 0.95,
+	150.0/360.0, 0.25, 0.95,
+	180.0/360.0, 0.25, 0.95,
+	210.0/360.0, 0.25, 0.95,
+	240.0/360.0, 0.25, 0.95,
+	270.0/360.0, 0.25, 0.95,
+	300.0/360.0, 0.25, 0.95,
+	330.0/360.0, 0.25, 0.95,
+	0, 0.00, 0.80, -- White Scale Right
+	0, 0.00, 0.70, -- White Scale Right
+	000.0/360.0, 0.5, 0.70, -- Washed Out
+	030.0/360.0, 0.50, 0.70,
+	060.0/360.0, 0.50, 0.70,
+	090.0/360.0, 0.50, 0.70,
+	120.0/360.0, 0.50, 0.70,
+	150.0/360.0, 0.50, 0.70,
+	180.0/360.0, 0.50, 0.70,
+	210.0/360.0, 0.50, 0.70,
+	240.0/360.0, 0.50, 0.70,
+	270.0/360.0, 0.50, 0.70,
+	300.0/360.0, 0.50, 0.70,
+	330.0/360.0, 0.50, 0.70,
+	0, 0.00, 0.60, -- White Scale Right
+	0, 0.00, 0.50, -- White Scale Right
+	000.0/360.0, 0.75, 0.5, -- Dark Washed Out
+	030.0/360.0, 0.75, 0.5,
+	060.0/360.0, 0.75, 0.5,
+	090.0/360.0, 0.75, 0.5,
+	120.0/360.0, 0.75, 0.5,
+	150.0/360.0, 0.75, 0.5,
+	180.0/360.0, 0.75, 0.5,
+	210.0/360.0, 0.75, 0.5,
+	240.0/360.0, 0.75, 0.5,
+	270.0/360.0, 0.75, 0.5,
+	300.0/360.0, 0.75, 0.5,
+	330.0/360.0, 0.75, 0.5,
+	0, 0.00, 0.40, -- White Scale Right
+	0, 0.00, 0.30, -- White Scale Right
+	000.0/360.0, 0.95, 0.80, -- Pure
+	030.0/360.0, 0.95, 0.80,
+	060.0/360.0, 0.95, 0.80,
+	090.0/360.0, 0.95, 0.80,
+	120.0/360.0, 0.95, 0.80,
+	150.0/360.0, 0.95, 0.80,
+	180.0/360.0, 0.95, 0.80,
+	210.0/360.0, 0.95, 0.80,
+	240.0/360.0, 0.95, 0.80,
+	270.0/360.0, 0.95, 0.80,
+	300.0/360.0, 0.95, 0.80,
+	330.0/360.0, 0.95, 0.80,
+	0, 0.00, 0.20, -- White Scale Right
+	0, 0.00, 0.10, -- White Scale Right
+	000.0/360.0, 0.95, 0.35, -- Dark
+	030.0/360.0, 0.95, 0.35,
+	060.0/360.0, 0.95, 0.35,
+	090.0/360.0, 0.95, 0.35,
+	120.0/360.0, 0.95, 0.35,
+	150.0/360.0, 0.95, 0.35,
+	180.0/360.0, 0.95, 0.35,
+	210.0/360.0, 0.95, 0.35,
+	240.0/360.0, 0.95, 0.35,
+	270.0/360.0, 0.95, 0.35,
+	300.0/360.0, 0.95, 0.35,
+	330.0/360.0, 0.95, 0.35
+}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_BAD = {0.65, 0, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_GOOD = {0, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_AVERAGE = {0.65, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_NEUTRAL = {130.0/255, 130.0/255, 130.0/255, 1}
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 250.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 250
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 250
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_GOOD = {0.0, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_AVERAGE = {0.65, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_BAD = {0.65, 0, 0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_NAVY_COLOR_MISSION = {0.65, 0.65, 0.0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_NAVY_COLOR_NEUTRAL = {130.0/255, 130.0/255, 130.0/255, 1}
+NDefines_Graphics.NGraphics.ROOT_FRONT_OFFSET = 2
+
+for k, v in pairs(NDefines_Graphics) do NDefines[k] = v end
